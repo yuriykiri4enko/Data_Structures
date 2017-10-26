7049cf21007c1246cd5304f9514a3009e22402e0
sadman <yuriykiri4enko@gmail.com>
2017-10-26 23:56:09 +0300

    Реализация неупорядоченного списка ( связанные списки )

---
+++ Unordered_List/Node.py
@@ -0,0 +1,17 @@
+#Узел списка представленный значением
+# и ссылкой на следующий узел (initdata, next)
+
+
+class Node:
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+    def getData(self):
+        return self.data
+    def getNext(self):
+        return self.next
+    def setData(self, newdata):
+        self.data = newdata
+    def setNext(self, newnext):
+        self.next = newnext
+
---
+++ Unordered_List/UnorderedList.py
@@ -0,0 +1,61 @@
+from Unordered_List import Node as nd
+
+class UnorderdList:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self, item):
+        # nd это ссылка на файл Node.py , а .Node это класс
+        temp = nd.Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count += 1
+            current = current.getNext()
+
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+
+if __name__ == '__main__':
+    mylist = UnorderdList()
+
+    mylist.add(31)
+    mylist.add(77)
+    mylist.add(17)
+    mylist.add(26)
+    print(mylist.search(17))
+
+
