09979691dfa34fe678455db9efc8791ad095474d
sadman <yuriykiri4enko@gmail.com>
2017-10-26 22:49:25 +0300

    Реализация дека (двусторонней очередью)

---
+++ Deque.py
@@ -0,0 +1,55 @@
+'''
+class Deque:
+    def __init__(self):
+        self.items = []
+    def isEmpty(self):
+        return self.items == []
+    def addFront(self, item):
+        self.items.append(item)
+    def addRear(self, item):
+        self.items.insert(0, item)
+    def removeFront(self):
+        return self.items.pop()
+    def removeRear(self):
+        return self.items.pop(0)
+    def size(self):
+        return len(self.items)
+
+
+d = Deque()
+print(d.isEmpty())
+d.addRear(4)
+d.addRear('dog')
+d.addFront('cat')
+d.addFront(True)
+print(d.size())
+print(d.isEmpty())
+d.addRear(8.5)
+print(d.removeFront())
+print(d.removeRear())
+'''
+
+
+#Проверка палиндрома
+#Палиндромом называют строку, которая одинаково
+# читается справа налево и слева направо
+from pythonds.basic.deque import Deque
+
+def palchecker(aString):
+    chardeque = Deque()
+
+    for ch in aString:
+        chardeque.addRear(ch)
+
+    stillEqual = True
+
+    while chardeque.size() > 1 and stillEqual:
+        first = chardeque.removeFront()
+        last = chardeque.removeRear()
+        if first != last:
+            stillEqual = False
+
+    return stillEqual
+
+print(palchecker("lsdkjfskf"))
+print(palchecker("radar"))
